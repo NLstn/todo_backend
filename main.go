c4ef20c55ef45355af6fd0a3ae50150f8d63566b
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2024-05-20 23:02:57 +0200

    Save todos to mongodb instead of local

--- main.go
+++ main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync"
-	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Description string             `json:"description"`
 }
 
-var idCounter int32
-var todos []Todo
 var mutex = &sync.Mutex{}
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI not set in .env file")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
+
+	collection := client.Database("todos").Collection("todos")
 
 	mux.HandleFunc("POST /v1/todos", func(w http.ResponseWriter, r *http.Request) {
 
@@ -31,11 +54,14 @@ func main() {
 		}
 		defer r.Body.Close()
 
-		todo.ID = int(atomic.AddInt32(&idCounter, 1))
-
 		mutex.Lock()
-		todos = append(todos, todo)
+		result, err := collection.InsertOne(context.TODO(), bson.M{"description": todo.Description})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		mutex.Unlock()
+		todo.ID = result.InsertedID.(primitive.ObjectID)
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
@@ -44,75 +70,100 @@ func main() {
 
 	mux.HandleFunc("GET /v1/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
 
-		id := r.PathValue("id")
-		if id == "" {
+		idStr := r.PathValue("id")
+		if idStr == "" {
 			http.Error(w, "id is required", http.StatusBadRequest)
 			return
 		}
 
-		for _, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(todo)
-				return
-			}
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			http.Error(w, "id must be an integer", http.StatusBadRequest)
+			return
 		}
 
-		http.Error(w, "todo not found", http.StatusNotFound)
+		var todo Todo
+		err = collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&todo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todo)
 	})
 
 	mux.HandleFunc("PATCH /v1/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		if id == "" {
-			http.Error(w, "id is required", http.StatusBadRequest)
+		id, err := primitive.ObjectIDFromHex(r.PathValue("id"))
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
 			return
 		}
 
-		var updatedTodo Todo
-		err := json.NewDecoder(r.Body).Decode(&updatedTodo)
+		var todo Todo
+		err = json.NewDecoder(r.Body).Decode(&todo)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		defer r.Body.Close()
 
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				mutex.Lock()
-				if updatedTodo.Description != "" {
-					todos[i].Description = updatedTodo.Description
-				}
-				mutex.Unlock()
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(todos[i])
-				return
-			}
-		}
-
-		http.Error(w, "todo not found", http.StatusNotFound)
-	})
-
-	mux.HandleFunc("DELETE /v1/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		if id == "" {
-			http.Error(w, "id is required", http.StatusBadRequest)
+		var updatedTodo Todo
+		err = collection.FindOneAndUpdate(
+			context.TODO(),
+			bson.M{"_id": id},
+			bson.M{"$set": bson.M{"description": todo.Description}},
+			options.FindOneAndUpdate().SetReturnDocument(options.After),
+		).Decode(&updatedTodo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				mutex.Lock()
-				todos = append(todos[:i], todos[i+1:]...)
-				mutex.Unlock()
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(updatedTodo)
+	})
+
+	mux.HandleFunc("DELETE /v1/todos/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, err := primitive.ObjectIDFromHex(r.PathValue("id"))
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
 		}
 
-		http.Error(w, "todo not found", http.StatusNotFound)
+		result, err := collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if result.DeletedCount == 0 {
+			http.Error(w, "todo not found", http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
 	})
 
 	mux.HandleFunc("GET /v1/todos", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := collection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close(context.TODO())
+
+		var todos []Todo
+		for rows.Next(context.TODO()) {
+			var todo Todo
+			err := rows.Decode(&todo)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			todos = append(todos, todo)
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(todos)
 	})
@@ -120,5 +171,4 @@ func main() {
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
